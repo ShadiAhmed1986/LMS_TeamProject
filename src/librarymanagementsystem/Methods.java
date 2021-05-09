d43ee77b1cb1d52312a092e0813681a3bbcebd2e
erkal38 <nalruvceysem1984@gmail.com>
2021-05-09 19:38:02 +0300

    erkal

---
+++ src/librarymanagementsystem/Methods.java
@@ -0,0 +1,60 @@
+package librarymanagementsystem;
+
+import java.util.Scanner;
+
+public class Methods extends PasswordAndUsernameValidation{
+
+	Scanner scan = new Scanner(System.in);
+		
+	public void register(String names[], RegistrationForm regForm){
+		System.out.println("Please enter your first name!");
+		regForm.setFirstName(scan.next());
+		System.out.println("Please enter your last name!");
+		regForm.setLastName(scan.next());
+		System.out.println("===========================================");
+		while (!passwordValidation(passwordDatabase)) {
+			passwordValidation(passwordDatabase);		
+		};
+		System.out.println("===========================================");
+		while(!usernameValidation()) {
+			usernameValidation();
+		};
+		System.out.println("===========================================");
+		System.out.println("---=== Registration successfull ! ===---\nThank you for registering " 
+                +regForm.getFirstName().toUpperCase() 
+                + " " + regForm.getLastName().toUpperCase());
+		System.out.println("===========================================");
+		System.out.println("Please Log In using your username and password !");
+		regForm.setRegistered(true);
+		login(names,regForm);
+		
+	}
+	
+	public void checkIfCustomer(RegistrationForm regForm) {
+		System.out.println("Are you a member ?(yes/no)");
+		String answer = scan.next();
+		if(answer.equals("yes")){
+			regForm.setMember(true);
+		}else {
+			regForm.setMember(false);
+		}
+	}
+	
+	public void login(String names[], RegistrationForm regForm) {
+		String name = scan.next();
+		int count = 0;
+		for (int i = 0; i < names.length; i++) {
+			if(names[i].equalsIgnoreCase(name)) {
+				System.out.println("Log in succesfull !");
+				break;
+			}else {
+			}
+			count++;		
+		}
+		if(count > 0) {
+			System.out.println("You are not registered !");
+			register(names,regForm);
+		}
+	}
+	
+}
--- src/librarymanagementsystem/Runner.java
+++ src/librarymanagementsystem/Runner.java
@@ -3,14 +3,35 @@ package librarymanagementsystem;
 public class Runner {
 
 	public static void main(String[] args) {
-		Search obj=new Search();
+
+		String names[] = { "Emily","erkal", "Hannah", "Madison", "Ashley", "Sarah", "Alexis", "Samantha", "Jessica",
+				"Elizabeth", "Taylor", "Lauren", "Alyssa", "Kayla", "Abigail", "Brianna", "Olivia", "Emma", "Megan",
+				"Grace", "Victoria", "Rachel", "Anna", "Sydney", "Destiny", "Morgan", "Jennifer", "Jasmine", "Haley",
+				"Julia", "Kaitlyn", "Nicole", "Amanda", "Katherine", "Natalie", "Hailey", "Alexandra", "Savannah",
+				"Chloe", "Rebecca", "Stephanie", "Maria", "Sophia", "Mackenzie", "Allison", "Isabella", "Amber", "Mary",
+				"Danielle", "Gabrielle", "Jordan", "Brooke", "Michelle", "Sierra", "Katelyn", "Andrea", "Madeline",
+				"Sara", "Kimberly", "Courtney", "Erin", "Brittany", "Vanessa", "Jenna", "Jacqueline", "Caroline",
+				"Faith", "Makayla", "Bailey", "Paige" };
+
+		RegistrationForm regForm = new RegistrationForm();
+		Methods methods = new Methods();
+
+		System.out.println("-----===== Welcome to our Library =====-----");
+		methods.checkIfCustomer(regForm);
+
+		if (!regForm.isMember) {
+			System.out.println("-----===== Please Register First =====-----");
+			methods.register(names, regForm);
+		} else {
+			System.out.println("Great !!! Please Log In using your username and password !");
+			methods.login(names, regForm);
+		}
+		Search obj = new Search();
 		obj.chooseAnOption();
-		//obj.selectOption();
-		//obj.searchBook();
-        //obj.searchByAuthor();
-        //obj.searchByGenres();
-		
-		
-		
+		// obj.selectOption();
+		// obj.searchBook();
+		// obj.searchByAuthor();
+		// obj.searchByGenres();
+
 	}
 }
